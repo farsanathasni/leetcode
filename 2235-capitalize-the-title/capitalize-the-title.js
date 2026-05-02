8459ad19a13aac2e8055bdbabad5428be1356ae5
farsanathasni <thasnifarsana294@gmail.com>
2026-05-02 16:20:40 +0530

    Time: 0 ms (100.00%) | Memory: 55.8 MB (12.64%) - LeetSync

---
+++ 2235-capitalize-the-title/capitalize-the-title.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} title
+ * @return {string}
+ */
+var capitalizeTitle = function(title) {
+    return title
+        .toLowerCase()
+        .split(" ")
+        .map(word => {
+            if (word.length <= 2) return word;
+            return word[0].toUpperCase() + word.slice(1);
+        })
+        .join(" ");
+        };
